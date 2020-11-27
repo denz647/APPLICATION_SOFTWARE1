942564c2dc646702c93d208fcbaab05303e57fd3
Dennis thomas <52597699+denz647@users.noreply.github.com>
2020-11-27 15:29:38 +0530

    Add files via upload

---
+++ DENNIS_LAB4.sql
@@ -0,0 +1,16 @@
+use employeedetails;
+
+create table department(code int primary key,
+Title char(20),Dept_name char(20) unique,Dept_id varchar(10),
+salary int CHECK(salary>2000));
+
+insert into department values(1,"A","CS Department","Cs301",4500);
+insert into department values(2,"B","EC Department","EC344",7000);
+
+create table Instructor(Name char(20) not null,Code varchar(20) ,Id int default 0000);
+
+insert into Instructor values("Alok","EC142",2001);
+insert into Instructor values("Ashok","CS665",2002);
+ 
+ drop table department;
+ drop table Instructor;
