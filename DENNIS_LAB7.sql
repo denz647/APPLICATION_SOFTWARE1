8a825389fd0a94618bac2fac297a68ec092b4f48
Dennis thomas <52597699+denz647@users.noreply.github.com>
2020-12-18 16:01:05 +0530

    Update DENNIS_LAB7.sql

--- DENNIS_LAB7.sql
+++ DENNIS_LAB7.sql
@@ -1,16 +1,20 @@
 use wholesales;
+-- Qa)create a table store
 create table store(order_no int primary key,
 code int,item varchar(20),quantity int,price int,
 discount int,mrp int);
 
+--Qb)enter values to table
 insert into store (order_no,code,item,quantity,price,discount,mrp)
 values(01,1101,"bread",2,150,10,160),(02,1102,"oats",5,300,10,310);
 
+--Qc)display the table
 select * from store;
 
+--Qd)
 select MOD(price,9) from store;
-
+--Qe)
 select price,POWER(price,2) from store;
-
+--Qf)
 select ROUND(mrp DIV 7) from store;
 
