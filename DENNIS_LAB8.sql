332409382196bf3eab0846f17f85901819eee898
Dennis thomas <52597699+denz647@users.noreply.github.com>
2021-01-08 15:47:51 +0530

    Update DENNIS_LAB8.sql

--- DENNIS_LAB8.sql
+++ DENNIS_LAB8.sql
@@ -1,3 +1,4 @@
+--Question No:1
 CREATE TABLE Employee(
     code CHAR(8) PRIMARY KEY,
     name VARCHAR(40),
@@ -6,7 +7,7 @@ CREATE TABLE Employee(
     salary FLOAT
 );
 
-
+--Question No:2
 INSERT INTO Employee (code, name, dob, designation, salary)
 VALUES
 ('Hr112','Sam','1999-07-14','HR',45000.50),
@@ -14,17 +15,17 @@ VALUES
 ('QA878','Kishore','1999-02-06','Quality analyst',25000),
 ('cl789','Arun','2000-02-05','Clerk',12000.75);
 
-
+--Question No:3
 SELECT SUM(salary) FROM Employee WHERE designation='Clerk';
 
-
+--Question No:4
 SELECT MAX(salary) FROM Employee;
 
-
+--Question No:5
 SELECT AVG(salary) FROM Employee;
 
-
+--Question No:6
 SELECT MIN(salary) FROM Employee;
 
-
+--Question No:7
 SELECT COUNT(*) FROM Employee;
