f417da153b1d5f0a94cceab9bf45c787cbb19009
Dennis thomas <52597699+denz647@users.noreply.github.com>
2021-01-24 23:57:32 +0530

    Update DENNIS_LAB12.sql

--- DENNIS_LAB12.sql
+++ DENNIS_LAB12.sql
@@ -1,3 +1,15 @@
+--EXP 12
+--implementation of join operator
+--Consider the following schema: 
+ --Sailors (sid, sname, rating, age) 
+ --Boats (bid, bname, color) 
+ --Reserves (sid, bid, day(date)) 
+--1. Find all information of sailors who have reserved boat number 101. 
+--2. Find the name of boat reserved by Bob. 
+--3. Find the names of sailors who have reserved a red boat, and list in the order of age. 
+--4. Find the names of sailors who have reserved at least one boat. 
+--5. Find the ids and names of sailors who have reserved two different boats on the same day
+
 use employee;
 Create Table Sailors
 (sid int primary key,
